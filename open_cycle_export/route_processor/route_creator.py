782aa29b17913ddfe5a593617c77a1f9215ff647
Henry Miskin <hpgmiskin@gmail.com>
2020-01-14 22:18:03 +0000

    Implement basic Dijkstra's algorithm for routing

--- open_cycle_export/route_processor/route_creator.py
+++ open_cycle_export/route_processor/route_creator.py
@@ -5,125 +5,102 @@ Waypoint - Index of point at start or end of a line
 
 """
 
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Tuple
 
-import itertools
+import numpy
 
 Waypoint = int
 Waypoints = List[Waypoint]
-
+WaypointSet = Set[Waypoint]
 ConnectedWaypoints = Dict[Waypoint, Waypoints]
 WaypointDistances = List[List[float]]
-
-WaypointSet = Set[Waypoint]
-
-
-class NoRouteError(Exception):
-    pass
-
-
-def add_waypoint(current_set, *new_items):
-    return current_set | set(new_items)
 
 
 def route_creator(
     waypoints: Waypoints,
     connected_waypoints: ConnectedWaypoints,
-    waypoint_distances: WaypointDistances,
-    number_close_waypoint_search: int = 10,
+    vertex_distances: WaypointDistances,
+    euclidean_distances: WaypointDistances,
 ):
     """Returns a function used to create a route between two points
     
     Arguments:
         waypoints {Waypoints} -- Waypoints available to use for navigation
-        connected_waypoints {ConnectedWaypoints} -- Lookup table connected waypoints
-        waypoint_distances {WaypointDistances} -- Distance between each waypoint
+        neighbor_distances {NeighborDistances} -- Path distance to neighbor waypoints
+        euclidean_distances {WaypointDistances} -- Straight line between each waypoint
     
     Returns:
         create_route -- Function to create a route between two locations
     """
 
-    def create_route(
-        start_point: Waypoint,
-        end_point: Waypoint,
-        visited_waypoints: WaypointSet = set(),
-    ):
+    waypoints = numpy.array(waypoints)
+    vertex_distances = numpy.array(vertex_distances)
+    euclidean_distances = numpy.array(euclidean_distances)
+
+    def create_route(start_waypoint: Waypoint, end_waypoint: Waypoint):
         """Create a route between two places
         
         Arguments:
-            start_point {Waypoint} -- Start waypoint to generate route from
-            end_point {Waypoint} -- End waypoint to find route too
-            visited_waypoints {WaypointSet} -- Set of waypoints already visited
+            start_waypoint {Waypoint} -- Start waypoint to generate route from
+            end_waypoint {Waypoint} -- End waypoint to find route too
         
         Returns:
             route {Waypoints} -- List of waypoints which make a route
-            unconnected_distance {float} -- Distance 
         """
 
-        # Base case where route complete
-        if start_point == end_point:
-            return [end_point], 0
+        waypoint_parents = {}
+        unvisited_waypoints = set(waypoints)
+        min_distances = numpy.full(len(waypoints), numpy.inf)
+        min_distances[start_waypoint] = 0
+        current_waypoint = start_waypoint
 
-        def find_distance(next_point):
-            if start_point == next_point:
-                return max(waypoint_distances[start_point])
-            return waypoint_distances[start_point][next_point]
+        # Find minimum distances from start waypoint until end waypoint is current minimum
 
-        def find_route(next_waypoint, current_unconnected_distance=0):
-            updated_visited = add_waypoint(visited_waypoints, start_point)
-            route, unconnected_distance = create_route(
-                next_waypoint, end_point, updated_visited
-            )
-            return (
-                [start_point, *route],
-                current_unconnected_distance + unconnected_distance,
-            )
+        while current_waypoint != end_waypoint:
 
-        # List of directly connected waypoints
-        directly_connected_waypoints = connected_waypoints[start_point]
+            unvisited_waypoints.remove(current_waypoint)
 
-        # All unvisited waypoints which are connected to the start point
-        connected_unvisited_waypoints = [
-            connected_waypoint
-            for connected_waypoint in directly_connected_waypoints
-            if connected_waypoint not in visited_waypoints
-        ]
-
-        # Compare distance from start point to end point with distance to start point
-        current_distance = waypoint_distances[start_point][end_point]
-        closer_connected_waypoints = [
-            next_waypoint
-            for next_waypoint in connected_unvisited_waypoints
-            if waypoint_distances[next_waypoint][end_point] < current_distance
-        ]
-
-        # If there are directly connected waypoints we chose the route with least unconnected
-        if len(closer_connected_waypoints):
-            return min(
+            current_waypoint_distances = euclidean_distances[current_waypoint] * 1000
+            current_connected_waypoints = connected_waypoints[current_waypoint]
+            current_vertex_distances = numpy.array(
                 [
-                    find_route(next_waypoint)
-                    for next_waypoint in closer_connected_waypoints
-                ],
-                key=lambda result: result[1],
+                    vertex_distances[current_waypoint][next_waypoint]
+                    for next_waypoint in current_connected_waypoints
+                ]
             )
 
-        # If there are no connected and unvisited waypoints find all unvisited waypoints
-        unvisited_waypoints = [
-            waypoint for waypoint in waypoints if waypoint not in visited_waypoints
-        ]
+            numpy.put(
+                current_waypoint_distances,
+                current_connected_waypoints,
+                current_vertex_distances,
+            )
 
-        # Create list of a couple of closest unvisited waypoint
-        closest_unvisited_waypoints = itertools.islice(
-            sorted(unvisited_waypoints, key=find_distance), number_close_waypoint_search
-        )
+            distance_to_current = min_distances[current_waypoint]
+            distances_from_start = distance_to_current + current_waypoint_distances
+            is_closer_from_here = distances_from_start < min_distances
+            child_indexes = numpy.flatnonzero(is_closer_from_here)
 
-        # Select the route with the least
-        return min(
-            [
-                find_route(next_waypoint, find_distance(next_waypoint))
-                for next_waypoint in closest_unvisited_waypoints
-            ],
-            key=lambda result: result[1],
-        )
+            waypoint_parents.update(
+                {child_index: current_waypoint for child_index in child_indexes}
+            )
+
+            min_distances = numpy.where(
+                is_closer_from_here, distances_from_start, min_distances
+            )
+
+            unvisited_waypoints_array = numpy.array(list(unvisited_waypoints))
+            is_unvisited = numpy.isin(waypoints, unvisited_waypoints_array)
+            unvisited_distances = numpy.where(is_unvisited, min_distances, numpy.inf)
+            current_waypoint = numpy.argmin(unvisited_distances)
+
+        # Build the route by traversing the parents backwards
+
+        reverse_route = [end_waypoint]
+        current_waypoint = end_waypoint
+        while current_waypoint != start_waypoint:
+            current_waypoint = waypoint_parents[current_waypoint]
+            reverse_route.append(current_waypoint)
+
+        return reverse_route[-1::-1]
 
     return create_route
--- open_cycle_export/route_processor/tests/test_route_creator.py
+++ open_cycle_export/route_processor/tests/test_route_creator.py
@@ -6,8 +6,9 @@ from open_cycle_export.route_processor.route_creator import route_creator
 def basic_route_data():
     waypoints = [0, 1, 2]
     connected_waypoints = {0: [1], 1: [0, 2], 2: [1]}
+    # vertex_distances = {(0, 1): 2, (1, 0): 2, (1, 2): 2, (2, 1): 2}
     waypoint_distances = [[0, 2, 4], [2, 0, 2], [4, 2, 0]]
-    return waypoints, connected_waypoints, waypoint_distances
+    return waypoints, connected_waypoints, waypoint_distances, waypoint_distances
 
 
 def disconnected_route_data():
@@ -21,7 +22,7 @@ def disconnected_route_data():
         [5, 3, 1, 0, 2],
         [7, 5, 4, 2, 0],
     ]
-    return waypoints, connected_waypoints, waypoint_distances
+    return waypoints, connected_waypoints, waypoint_distances, waypoint_distances
 
 
 class TestRouteCreator(unittest.TestCase):
@@ -30,34 +31,24 @@ class TestRouteCreator(unittest.TestCase):
     def test_basic_base_case(self):
         "Route between same start and end should only contain single waypoint"
         create_route = route_creator(*basic_route_data())
-        route, unconnected_distance = create_route(2, 2)
-        self.assertListEqual(route, [2])
-        self.assertEqual(unconnected_distance, 0)
+        self.assertListEqual(create_route(2, 2), [2])
 
     def test_basic_single_connected_route(self):
         "Waypoints directly connected should be returned as two element list"
         create_route = route_creator(*basic_route_data())
-        route, unconnected_distance = create_route(0, 1)
-        self.assertListEqual(route, [0, 1])
-        self.assertEqual(unconnected_distance, 0)
+        self.assertListEqual(create_route(0, 1), [0, 1])
 
     def test_basic_connected_route(self):
         "Waypoints directly connected should be returned as three element list"
         create_route = route_creator(*basic_route_data())
-        route, unconnected_distance = create_route(0, 2)
-        self.assertListEqual(route, [0, 1, 2])
-        self.assertEqual(unconnected_distance, 0)
+        self.assertListEqual(create_route(0, 2), [0, 1, 2])
 
     def test_disconnected_waypoints_jump_gap(self):
         "Waypoints which are close but there is no connection should return connection"
         create_route = route_creator(*disconnected_route_data())
-        route, unconnected_distance = create_route(2, 3)
-        self.assertListEqual(route, [2, 3])
-        self.assertEqual(unconnected_distance, 1)
+        self.assertListEqual(create_route(2, 3), [2, 3])
 
     def test_disconnected_waypoints_full_route(self):
         "Waypoints which are close but there is no connection should return route"
         create_route = route_creator(*disconnected_route_data())
-        route, unconnected_distance = create_route(0, 4)
-        self.assertListEqual(route, [0, 1, 2, 3, 4])
-        self.assertEqual(unconnected_distance, 1)
+        self.assertListEqual(create_route(0, 4), [0, 1, 2, 3, 4])
