00f171f162ff3f49fc890a7df9897f56006899fa
Henry Miskin <hpgmiskin@gmail.com>
2020-01-25 17:52:51 +0000

    Create map plotter class to plot lines and points

--- open_cycle_export/map_builder/map_plotter.py
+++ open_cycle_export/map_builder/map_plotter.py
@@ -1,41 +1,56 @@
-from typing import List
+from typing import List, Tuple, Iterator
 
-from shapely.geometry import LineString, MultiLineString
+import operator
+import itertools
+
+from shapely.geometry import Point, LineString, MultiLineString
 
 import plotly.express
 import plotly.graph_objects
 
 
-def plot_multi_line_string(multi_line_string: MultiLineString):
+def get_lat_lon(coords: Iterator[Tuple[float, float]]):
+    coords_a, coords_b = itertools.tee(coords, 2)
+    get_lon, get_lat = operator.itemgetter(0), operator.itemgetter(1)
+    return dict(lon=list(map(get_lon, coords_a)), lat=list(map(get_lat, coords_b)))
 
-    origin = plotly.graph_objects.Scattermapbox(
-        mode="markers",
-        lon=[multi_line_string.centroid.x],
-        lat=[multi_line_string.centroid.y],
-        marker={"size": 10},
-    )
-    figure = plotly.graph_objects.Figure(origin)
 
-    for line_string in multi_line_string:
-        series = plotly.graph_objects.Scattermapbox(
-            mode="lines",
-            lon=[coord[0] for coord in line_string.coords],
-            lat=[coord[1] for coord in line_string.coords],
+class MapPlotter:
+
+    figure: plotly.graph_objects.Figure
+
+    def __init__(self):
+        self.figure = plotly.graph_objects.Figure()
+
+    def plot_multi_line_string(self, name: str, multi_line_string: MultiLineString):
+        coords = itertools.chain(*[line.coords for line in multi_line_string])
+        self._add_scattermapbox(name=name, mode="lines", **get_lat_lon(coords))
+
+    def plot_waypoints(self, name: str, waypoints: List[Point]):
+        coords = map(lambda point: (point.x, point.y), waypoints)
+        self._add_scattermapbox(name=name, mode="markers", **get_lat_lon(coords))
+
+    def show(self, center: Point):
+        self.figure.update_layout(
+            mapbox={
+                "center": {"lon": center.x, "lat": center.y},
+                "style": "stamen-terrain",
+                "zoom": 10,
+            }
         )
-        figure.add_trace(series)
+        self.figure.show()
 
-    figure.update_layout(
-        mapbox={
-            "center": {
-                "lon": multi_line_string.centroid.x,
-                "lat": multi_line_string.centroid.y,
-            },
-            "style": "stamen-terrain",
-            "zoom": 10,
-        }
-    )
+    def _add_scattermapbox(self, **kwargs):
+        trace = plotly.graph_objects.Scattermapbox(**kwargs)
+        self.figure.add_trace(trace)
 
-    figure.show()
+
+def plot_multi_line_string(multi_line_string: MultiLineString):
+    map_plotter = MapPlotter()
+    map_plotter.plot_multi_line_string("Ways", multi_line_string)
+    waypoints = [Point(line.coords[0]) for line in multi_line_string]
+    map_plotter.plot_waypoints("Waypoints", waypoints)
+    map_plotter.show(multi_line_string.centroid)
 
 
 def main():
